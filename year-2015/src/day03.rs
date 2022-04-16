cc354b45a3a5aff5a982071e9085b1629c07b292
Dariusz Dajka <dariusz.dajka@gmail.com>
2022-04-16 14:10:03 +0200

    Add solution to part 2

--- year-2015/src/day03.rs
+++ year-2015/src/day03.rs
@@ -48,44 +48,52 @@ impl Position {
 }
 
 fn houses_visited(input: &str) -> usize {
-    // let mut house_positions = HashSet::new();
-    // let mut santas_position = Position::default();
-    //
-    // house_positions.insert(santas_position);
-    //
-    // for symbol in input.chars() {
-    //     let direction = Direction::from(symbol);
-    //     santas_position.move_pos(direction);
-    //
-    //     house_positions.insert(santas_position);
-    // }
-    //
-    // house_positions.len()
+    let mut house_positions = HashSet::new();
+    let mut santas_position = Position::default();
 
-    let houses_visited: HashSet<_> = input
-        .chars()
-        .into_iter()
-        .scan(Position::default(), |current_position, symbol| {
-            let direction = Direction::from(symbol);
-            current_position.move_pos(direction);
+    house_positions.insert(santas_position);
 
-            Some(current_position.clone())
-        })
-        .chain(std::iter::once(Position::default())) // include the initial position as well
-        .collect();
+    for symbol in input.chars() {
+        let direction = Direction::from(symbol);
+        santas_position.move_pos(direction);
 
-    houses_visited.len()
+        house_positions.insert(santas_position);
+    }
+
+    house_positions.len()
 }
 
 fn houses_visited_with_robo_santa(input: &str) -> usize {
-    3
+    let mut house_positions = HashSet::new();
+    let mut santas_position = Position::default();
+    let mut robos_position = Position::default();
+
+    house_positions.insert(santas_position);
+
+    for (n, symbol) in input.chars().enumerate() {
+        let direction = Direction::from(symbol);
+
+        let new_position = if n % 2 == 0 {
+            santas_position.move_pos(direction);
+            santas_position
+        } else {
+            robos_position.move_pos(direction);
+            robos_position
+        };
+
+        house_positions.insert(new_position);
+    }
+
+    house_positions.len()
 }
 
 fn main() -> Result<()> {
     let input = read_single_input_line(Year(2015), Day(3))?;
     let houses_visited = houses_visited(&input);
+    let houses_visited_with_robo = houses_visited_with_robo_santa(&input);
 
     println!("Houses visited: {houses_visited}");
+    println!("Houses visited with Robo-Santa: {houses_visited_with_robo}");
 
     Ok(())
 }
@@ -135,5 +143,25 @@ mod tests {
         let houses_visited = houses_visited_with_robo_santa(input);
 
         assert_eq!(3, houses_visited);
+    }
+
+    // ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa
+    // end up back where they started.
+    #[test]
+    fn both_back_and_forth() {
+        let input = "^>v<";
+        let houses_visited = houses_visited_with_robo_santa(input);
+
+        assert_eq!(3, houses_visited);
+    }
+
+    // ^v^v^v^v^v now delivers presents to 11 houses, with Santa going
+    // one direction and Robo-Santa going the other
+    #[test]
+    fn both_going_away() {
+        let input = "^v^v^v^v^v";
+        let houses_visited = houses_visited_with_robo_santa(input);
+
+        assert_eq!(11, houses_visited);
     }
 }
